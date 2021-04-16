99612076690ab8462ea8df177923ede30af1b11d
AadhyaKocha <51734801+AadhyaKocha@users.noreply.github.com>
2021-04-15 21:19:09 -0400

    frontend setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let t = 0;
+
+window.setInterval(() => {
+  $('.timer').remove();
+  $('#main').append(
+    `
+        <p class='timer'>You've been on this page for ${t} seconds...</p>
+        `,
+  );
+  t += 1;
+}, 1000);
