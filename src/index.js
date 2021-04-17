20bcf3cab43692e1aa16e26294ef044c7270a869
AadhyaKocha <51734801+AadhyaKocha@users.noreply.github.com>
2021-04-17 15:08:06 -0400

    react starteradded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import $ from 'jquery';
 import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let t = 0;
 
