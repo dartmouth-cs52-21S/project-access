56cc10b93d2a9ea4d44666a8b41f90ddd0ddd40c
Vico Lee <vico.z.lee.24@dartmouth.edu>
2021-05-22 17:45:19 -0400

    first commit for landingpage

---
+++ src/components/landingpage.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+import React, { Component } from 'react';
+import '../style.scss';
+
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>This is the landing page</div>
+    );
+  }
+}
+
+export default LandingPage;
