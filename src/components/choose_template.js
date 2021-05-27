ccdaf98aa02cbd2b6360b578f740324c5d9bcad0
John Kariuki <56233847+johnbkariuki@users.noreply.github.com>
2021-05-26 20:55:52 -0400

    merge conflict

--- src/components/choose_template.js
+++ src/components/choose_template.js
@@ -8,29 +8,14 @@ class ChooseTemplate extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      chosenTemplate: null,
-    };
+    this.state = {};
   }
-
-  // componentDidMount = (props) => {
-  // store images from template options locally to render
-  // }
-
-    selectedtemplate = (id) => {
-      this.setState({ chosenTemplate: id });
-      this.props.saveTemplate(this.state.chosenTemplate);
-    }
 
     displaytemplates = () => {
       const templates = this.props.templateOptions.map((template, id) => {
         // style.scss to conform all templates to a same window size
-        <div id="templateobject" onClick={() => { this.selectedtemplate(id); }}>
-          <div className="background-">
-            <h2>{template.title}</h2>
-            {/* will specify the contents of body not to sure what is in it */}
-            <div>{template.body}</div>
-          </div>
+        <div className="choose-template-page-bottom">
+          <img src={template.url} alt="none" onClick={this.props.selectedtemplate(id)} />
         </div>;
         return (
           <ul id="choose-template-templates">
@@ -42,17 +27,11 @@ class ChooseTemplate extends Component {
 
     render() {
       // const DISPLAY = this.displaytemplates;
-      // get this from redux state
       return (
         // desigining the template page
         <div className="choose-template-page">
-          <div className="choose-template-page-top">
-            Choose Your Template
-          </div>
-          {/* {this.displaytemplates()} */}
-          <div className="choose-template-page-" />
+          {this.displaytemplates()}
         </div>
-        , { }
       );
     }
 }
