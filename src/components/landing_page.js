61490891045cc5e3be4be3768b1b906e56361ef7
Vico Lee <vico.z.lee.24@dartmouth.edu>
2021-05-29 16:26:36 -0400

    added landing page content

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import '../style.scss';
+import '../landing_page.scss';
+import {
+  NavLink,
+} from 'react-router-dom';
 
 class LandingPage extends Component {
   constructor(props) {
@@ -10,7 +13,28 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <div>This is the landing page</div>
+      <div className="landing-page">
+        <div className="header-container">
+          <p>Build your personal portfolio with a click of a button!</p>
+          <NavLink exact to="/signup">
+            <button type="button">Lets get Started</button>
+          </NavLink>
+        </div>
+        <div className="center-container">
+          <img src="src/img/landing_page/you_got_this.png" alt="" />
+          <div>
+            <p>You’re destined for big things, we are here to get you there!</p>
+            <p>Make a beautiful site to showcase all your amazing experiences</p>
+          </div>
+        </div>
+        <div className="bottom-container">
+          <p>Boost your applications and get your dream job now!</p>
+          <img src="src/img/landing_page/chat.png" alt="" />
+        </div>
+        <NavLink exact to="/signup">
+          <button type="button">Start Now</button>
+        </NavLink>
+      </div>
     );
   }
 }
