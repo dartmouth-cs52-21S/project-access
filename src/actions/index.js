e6f248faa4e2eb405fcfa4ecda3c7f1103470572
Aadhya Kocha <aadhya.n.kocha.22@dartmouth.edu>
2021-05-29 11:17:16 -0400

    fetch, update, delete actions, existing portfolio page created

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,17 +5,18 @@ const ROOT_URL = 'http://localhost:9090/api';
 // keys for actiontypes
 export const ActionTypes = {
   CREATE_PORTFOLIO: 'CREATE_PORTFOLIO',
-  FETCH_POST: 'FETCH_POST',
-  UPDATE_POST: 'UPDATE_POST',
-  DELETE_POST: 'DELETE_POST',
-  ERROR_FETCH_POST: 'ERROR_FETCH_POST',
+  FETCH_PORTFOLIOS: 'FETCH_PORTFOLIOS',
+  FETCH_PORTFOLIO: 'FETCH_PORTFOLIO',
+  UPDATE_PORTFOLIO: 'UPDATE_PORTFOLIO',
+  DELETE_PORTFOLIO: 'DELETE_PORTFOLIO',
+  // ERROR_FETCH_POST: 'ERROR_FETCH_POST',
   ERROR_SET: 'ERROR_SET',
   ERROR_CLEAR: 'ERROR_CLEAR',
   AUTH_USER: 'AUTH_USER',
   DEAUTH_USER: 'DEAUTH_USER',
   AUTH_ERROR: 'AUTH_ERROR',
-  FETCH_USERS: 'FETCH_USERS',
-  FETCH_USER: 'FETCH_USER',
+  // FETCH_USERS: 'FETCH_USERS',
+  // FETCH_USER: 'FETCH_USER',
 };
 
 export function authError(error) {
@@ -49,17 +50,61 @@ export function createPortfolio(templateId, {
   };
 }
 
-export function fetchPortfolio(templateId) {
+export function fetchPortfolio(portfolioId) {
   return (dispatch) => {
-    axios.get(`${ROOT_URL}/portfolio/${templateId}`)
+    axios.get(`${ROOT_URL}/portfolios/${portfolioId}`)
       .then((response) => {
-        dispatch({ type: ActionTypes.FETCH_POST, payload: response.data });
+        dispatch({ type: ActionTypes.FETCH_PORTFOLIO, payload: response.data });
         dispatch({ type: ActionTypes.ERROR_CLEAR, payload: '' });
       })
       .catch((error) => {
-        console.log('fetch one post error found');
+        console.log('fetch portfolio error found');
         console.log(error);
         dispatch({ type: ActionTypes.ERROR_SET, payload: error });
+      });
+  };
+}
+
+export function fetchPortfolios() {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/portfolios`)
+      .then((response) => {
+        dispatch({ type: ActionTypes.FETCH_PORTFOLIOS, payload: response.data });
+        dispatch({ type: ActionTypes.ERROR_CLEAR, payload: '' });
+      })
+      .catch((error) => {
+        console.log('fetch portfolios error found');
+        console.log(error);
+        dispatch({ type: ActionTypes.ERROR_SET, payload: error });
+      });
+  };
+}
+
+export function updatePortfolio(templates, portfolioId) {
+  return (dispatch) => {
+    axios.put(`${ROOT_URL}/posts/${portfolioId}}`, templates, { headers: { authorization: localStorage.getItem('token') } })
+      .then((response) => {
+        dispatch({ type: ActionTypes.FETCH_PORTFOLIO, payload: response.data });
+        dispatch({ type: ActionTypes.ERROR_CLEAR, payload: '' });
+      })
+      .catch((error) => {
+        console.log('update portfolio error found');
+        console.log(error);
+        dispatch({ type: ActionTypes.ERROR_SET, payload: error });
+      });
+  };
+}
+
+export function deletePortfolio(portfolioId, history) {
+  return (dispatch) => {
+    axios.delete(`${ROOT_URL}/posts/${portfolioId}`, { headers: { authorization: localStorage.getItem('token') } })
+      .then((response) => {
+        dispatch({ type: ActionTypes.DELETE_PORTFOLIO, payload: response.data });
+        history.push('/');
+      })
+      .catch((error) => {
+        console.log('delete portfolio error found');
+        console.log(error);
       });
   };
 }
@@ -100,11 +145,6 @@ export function signupUser({
       dispatch(authError(`Sign Up Failed: ${error.response.data}`));
     });
   };
-  // does an axios.post on the /signup endpoint (only difference from above)
-  // on success does:
-  //  dispatch({ type: ActionTypes.AUTH_USER });
-  //  localStorage.setItem('token', response.data.token);
-  // on error should dispatch(authError(`Sign Up Failed: ${error.response.data}`));
 }
 
 // deletes token from localstorage
--- src/components/existing_portfolios.js
+++ src/components/existing_portfolios.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ReactMarkdown from 'react-markdown';
+import { fetchPortfolios } from '../actions';
 import '../style.scss';
 
-class existingPortfolios extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+// copy pasted from Aadhya's code but does not function yet
 
-  render() {
+function existingPortfolios(props) {
+  useEffect(() => {
+    props.fetchPortfolios();
+  }, []);
+
+  const [searchWord, setSearch] = useState(''); // this is for searching if we want a search bar
+  // Learned from Thomas in CS52 TA hours
+  const onChangeHandler = (setter) => (e) => setter(e.target.value);
+
+  if (props.all == null) {
+    return (<div />);
+  } else {
     return (
-      <div>Your portfolios</div>
+      <div>
+        <input type="text" tabIndex={0} value={searchWord} onChange={onChangeHandler(setSearch)} placeholder="Search portfolio" />
+        <div id="post_div"> {
+        props.all.filter((template) => {
+          if (template.title.includes(searchWord)) {
+            return template;
+          }
+          return null;
+        }).map((template) => {
+          return (
+            <Link to={`posts/${template._id}`} className="post_link" key={template._id}>
+              <div className="template" key={template._id} id="template">
+                <ReactMarkdown>{template.title}</ReactMarkdown>
+              </div>
+            </Link>
+          );
+        })
+      }
+        </div>
+      </div>
     );
   }
 }
 
-export default existingPortfolios;
+function mapStateToProps(reduxState) {
+  return {
+    all: reduxState.template.all,
+  };
+}
+
+export default connect(mapStateToProps, { fetchPortfolios })(existingPortfolios);
