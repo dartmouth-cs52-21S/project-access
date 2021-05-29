9aef814e63b0225c9b96f5ae935fddd6b94945d4
Vico Lee <vico.z.lee.24@dartmouth.edu>
2021-05-29 13:00:03 -0400

    added portfolio component

---
+++ src/components/portfolio.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import '../style.scss';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchPortfolio } from '../actions';
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount = (props) => {
+    fetchPortfolio(this.props.match.params.id);
+  }
+
+  render() {
+    return (
+      <div>Renders the portfolio view page</div>
+    );
+  }
+}
+
+const mapStateToProps = (reduxState) => ({
+  curr: reduxState.portfolios.current,
+});
+
+export default withRouter(connect(mapStateToProps, { fetchPortfolio })(Portfolio));
