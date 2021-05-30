49c2cc338edf8cd2d7ebaca5c855d999f3784d21
Vico Lee <vico.z.lee.24@dartmouth.edu>
2021-05-29 23:45:37 -0400

    connected portfolio/id to backend

--- src/components/existing_portfolios.js
+++ src/components/existing_portfolios.js
@@ -1,53 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import ReactMarkdown from 'react-markdown';
-import { fetchPortfolios } from '../actions';
+// import React, { useEffect, useState } from 'react';
 import '../style.scss';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { fetchPortfolios } from '../actions';
 
-// copy pasted from Aadhya's code but does not function yet
+class ExistingPortfolios extends Component {
+  constructor(props) {
+    super(props);
 
-function ExistingPortfolios(props) {
-  useEffect(() => {
-    props.fetchPortfolios();
-  }, []);
+    this.state = {
+    };
+  }
 
-  const [searchWord, setSearch] = useState(''); // this is for searching if we want a search bar
-  // Learned from Thomas in CS52 TA hours
-  const onChangeHandler = (setter) => (e) => setter(e.target.value);
+  componentDidMount = (props) => {
+    this.props.fetchPortfolios();
+  }
 
-  if (props.all == null) {
-    return (<div />);
-  } else {
+  displayPortfolios = () => {
+    return (
+      this.props.portfolios.map((portfolio) => {
+        return (
+          <div>
+            Portfolio page
+          </div>
+        );
+      })
+    );
+  }
+
+  render() {
     return (
       <div>
-        <input type="text" tabIndex={0} value={searchWord} onChange={onChangeHandler(setSearch)} placeholder="Search portfolio" />
-        <div id="post_div"> {
-        props.all.filter((template) => {
-          if (template.title.includes(searchWord)) {
-            return template;
-          }
-          return null;
-        }).map((template) => {
-          return (
-            <Link to={`posts/${template._id}`} className="post_link" key={template._id}>
-              <div className="template" key={template._id} id="template">
-                <ReactMarkdown>{template.title}</ReactMarkdown>
-              </div>
-            </Link>
-          );
-        })
-      }
-        </div>
+        {this.displayPortfolios()}
       </div>
     );
   }
 }
 
-function mapStateToProps(reduxState) {
-  return {
-    all: reduxState.template.all,
-  };
-}
+// function ExistingPortfolios(props) {
+//   useEffect(() => {
+//     props.fetchPortfolios();
+//   }, []);
 
-export default connect(mapStateToProps, { fetchPortfolios })(ExistingPortfolios);
+//   // const [searchWord, setSearch] = useState(''); // this is for searching if we want a search bar
+//   // Learned from Thomas in CS52 TA hours
+//   // const onChangeHandler = (setter) => (e) => setter(e.target.value);
+
+//   if (props.all == null) {
+//     return (<div />);
+//   } else {
+//     return (
+//       <div>
+//         {/* <input type="text" tabIndex={0} value={searchWord} onChange={onChangeHandler(setSearch)} placeholder="Search portfolio name" /> */}
+//         <div id="post_div"> {
+//         props.all
+//         // .filter((portfolio) => {
+//           // if (portfolio.title.includes(searchWord)) {
+//           //   return portfolio;
+//           // }
+//           // return null;
+//         // })
+//           .map((portfolio) => {
+//             return (
+//               <Link to={`posts/${portfolio._id}`} className="post_link" key={portfolio._id}>
+//                 <div className="template" key={portfolio._id} id="template">
+//                   <ReactMarkdown>{portfolio.title}</ReactMarkdown>
+//                 </div>
+//               </Link>
+//             );
+//           })
+//       }
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const mapStateToProps = (state) => ({
+  portfolios: state.portfolio.all,
+});
+
+export default withRouter(connect(mapStateToProps, { fetchPortfolios })(ExistingPortfolios));
